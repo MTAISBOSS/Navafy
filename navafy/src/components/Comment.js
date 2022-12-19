079135b922161d1430ece35d306ec5e7ab9dce0d
aynr99 <aynourja@gmail.com>
2022-12-19 19:24:09 +0330

    comment finish

--- navafy/src/components/Comment.js
+++ navafy/src/components/Comment.js
@@ -11,36 +11,28 @@ import MapsUgcIcon from "@mui/icons-material/MapsUgc";
 
 import { Box, Grid } from "@mui/material";
 import CardActions from "@mui/material/CardActions";
-import {
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-} from "@mui/material";
+
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ConstructionRounded } from "@mui/icons-material";
 
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
 const notify = () => toast("Wow so easy!");
 
 const Card20s = () => {
-  const [userstate, LoggedIn] = useState(true);
+  const [userstate, setUserstate] = useState(true);
 
   function handleClick() {
     if (userstate == true) {
-      return toast.warn("truuuuuuuuue", {
-        position: "top-center",
-        autoClose: false,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
+      return setOpen(true);
     } else {
       return toast.warn("falseeeeeeee", {
         position: "top-center",
@@ -55,6 +47,12 @@ const Card20s = () => {
     }
   }
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Box dir="rtl">
       <Card>
@@ -63,6 +61,7 @@ const Card20s = () => {
             <CardActions>
               <IconButton aria-label="comment" onClick={handleClick}>
                 <ToastContainer
+                  open={!useState}
                   position="top-center"
                   autoclose={false}
                   newestOnTop
@@ -74,6 +73,28 @@ const Card20s = () => {
                 />
                 <MapsUgcIcon />
               </IconButton>
+              <Dialog open={open} onClose={handleClose} dir="rtl">
+                <DialogTitle>ثبت نظر</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    متن مورد نظر خود را وارد کنید:
+                  </DialogContentText>
+                  <TextField
+                    dir="rtl"
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="متن نظر"
+                    type="email"
+                    fullWidth
+                    variant="standard"
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose}>خروج</Button>
+                  <Button onClick={handleClose}>ارسال</Button>
+                </DialogActions>
+              </Dialog>
             </CardActions>
           </CardContent>
         </CardActionArea>
