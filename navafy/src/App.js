52aecd6f56c4a2d6fe04c2050d39e991de7c8068
aynr99 <aynourja@gmail.com>
2023-01-23 18:01:56 +0330

    artist profile router

--- navafy/src/App.js
+++ navafy/src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sign_up from "./components/Pages/Sign Up/Sign_up";
 import Artist_Sign_up from "./components/Pages/Artist Sign Up/Artist_Sign_Up";
 
-import MyHomePage from "./components/Pages/homepages/Artist_Homepage.jsx";
+import ArtistHompage from "./components/Pages/homepages/Artist_Homepage.jsx";
 //import MyHomePage from "./components/Comment";
-//import MyHomePage from "./components/Pages/homepages/Guest_Homepage.jsx";
+import MyHomePage from "./components/Pages/homepages/Guest_Homepage.jsx";
 //import Homepage from "./components/Pages/Home Page/Homepage";
 //import MyHomePage from "./components/Common/HomePage";
 //import MyHomePage from "./components/Pages/Home Page/HomePage.js";
@@ -26,6 +26,7 @@ function App() {
           <Route exact path="/signup" element={<Sign_up />} />
           <Route exact path="/artist_signup" element={<Artist_Sign_up />} />
           <Route exact path="/my_music_library" element={<Playlist />} />
+          <Route exact path="/artist_profile" element={<ArtistHompage />} />
         </Routes>
       </BrowserRouter>
     </div>
